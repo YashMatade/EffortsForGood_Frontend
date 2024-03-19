c308ca7707db1e2868a528f264d00eb35ce2370c
Yash Matade <105554335+YashMatade@users.noreply.github.com>
2024-03-19 12:17:23 +0530

    Update Strings.js

--- src/Routes/Strings.js
+++ src/Routes/Strings.js
@@ -1,6 +1,6 @@
 const Strings = {
     // API_ROUTE: "http://localhost:5000",
-    API_ROUTE: "https://a-ifor-life-backend.vercel.app"
+    API_ROUTE: "https://ai.myefg.org/api"
 };
 
 export default Strings;
